459836f7e38ceb7a46bf17b7fb9a1f2ed800937f
su1dar <dariyasuslina1@gmail.com>
2022-10-05 10:40:44 +0300

    сделала наведение на карточку и страницу карт, начала резиновую верстку

--- shop/js/menu.js
+++ shop/js/menu.js
@@ -1,10 +1,18 @@
-let button = document.getElementById('burger');
+let button = document.getElementById('button');
 
-let navigation = document.getElementById('menu');
+let navigation = document.getElementById('nuv');
 
 let wrp = document.getElementById('wrp');
 
 button.addEventListener('click', function(){ 
   navigation.classList.toggle('display-block');
-  wrp.classList.toggle('display-block');
-});
+ wrp.classList.toggle('display-block');
+
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+} //пробую оверлэй//
