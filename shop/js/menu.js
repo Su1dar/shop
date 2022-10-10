177452c1da4802f967bba52d8b2b1c1740f5511f
su1dar <dariyasuslina1@gmail.com>
2022-10-10 22:55:55 +0300

    заокнчила html, но меню,карточки и эффекты нужно доделать!!!

--- shop/js/menu.js
+++ shop/js/menu.js
@@ -1,18 +1,18 @@
-let button = document.getElementById('button');
+// let button = document.getElementById('button');
 
-let navigation = document.getElementById('nuv');
+// let navigation = document.getElementById('nuv');
 
-let wrp = document.getElementById('wrp');
+// let wrp = document.getElementById('wrp');
 
-button.addEventListener('click', function(){ 
-  navigation.classList.toggle('display-block');
- wrp.classList.toggle('display-block');
+// button.addEventListener('click', function(){ 
+//   navigation.classList.toggle('display-block');
+//  wrp.classList.toggle('display-block');
 
 
-function on() {
-  document.getElementById("overlay").style.display = "block";
-}
+// function on() {
+//   document.getElementById("overlay").style.display = "block";
+// }
 
-function off() {
-  document.getElementById("overlay").style.display = "none";
-} //пробую оверлэй//
+// function off() {
+//   document.getElementById("overlay").style.display = "none";
+// }
